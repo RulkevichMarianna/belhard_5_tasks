d38cb82602fae2301c911c73d2b1a02a3c517be6
Maryana Rulkevich <obrazcova.m@gmail.com>
2022-01-30 18:12:55 +0300

    done is_triangle

--- tasks/easy/if_elif_else/is_triangle.py
+++ tasks/easy/if_elif_else/is_triangle.py
@@ -17,8 +17,11 @@ is_rectangle(10, 4, 5) -> False
 
 
 def is_triangle(side1: int, side2: int, side3: int) -> bool:
-    result = None
-    return result
+
+    if side1 < (side2 + side3) and side2 < (side1 + side3) and side3 < (side2 + side3):
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
