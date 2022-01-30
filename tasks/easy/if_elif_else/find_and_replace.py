be72af995075efe5c995e05c6a70cf5c7316c0a9
Maryana Rulkevich <obrazcova.m@gmail.com>
2022-01-30 15:37:06 +0300

    done find_and_replace

--- tasks/easy/if_elif_else/find_and_replace.py
+++ tasks/easy/if_elif_else/find_and_replace.py
@@ -22,6 +22,11 @@ def find_and_replace(check_str: str, search_str: str) -> tuple:
     :return: обработанные строки
     :rtype: tuple
     """
+    search_str1 = search_str.lower()
+    check_str1 = check_str.lower()
+    if search_str1 in check_str1:
+        check_str = check_str.swapcase()
+        search_str = search_str.title()
     return check_str, search_str
 
 
