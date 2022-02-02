19631087aa1506dc0df8e75ea541d26fef98f1e6
Maryana Rulkevich <obrazcova.m@gmail.com>
2022-02-02 16:56:19 +0300

    done range

--- tasks/medium/range.py
+++ tasks/medium/range.py
@@ -15,8 +15,12 @@ VALUE_LIST = ['a', 'b', 'c', 'd', 'e', 'f']
 
 def list_compose(indexes: list, values: list) -> list:
     result_list = []
+    for i in indexes:
+        try:
+            result_list.append(values[i])
+        except IndexError:
+            result_list.append(None)
     return result_list
-
 
 if __name__ == '__main__':
     assert list_compose(INDEX_LIST, VALUE_LIST) == ['b', 'f', None, None, 'c', 'd',
